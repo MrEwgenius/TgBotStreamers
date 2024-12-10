665a48f810631e6f60536fff621c733b54706692
Bolynskii Ewgenii <119881162+MrEwgenius@users.noreply.github.com>
2024-12-10 16:35:01 +0300

    eddit

--- src/components/InputGroup/InputGroup.jsx
+++ src/components/InputGroup/InputGroup.jsx
@@ -3,11 +3,16 @@ import styles from "./InputGroup.module.scss";
 
 const InputGroup = ({ name, formData, handleChange, errors, label }) => {
   return (
-    <div key={name} className={`${styles.field} ${errors[name] ? styles.errorInput : ''}`}>
+    <div
+      key={name}
+      className={`${styles.field} ${errors[name] ? styles.errorInput : ""}`}
+    >
       <input
         type={name === "streamerLink" ? "url" : "number"}
         id={name}
+        inputMode="decimal"
         name={name}
+        pattern={name === "streamerLink" ? "" : "\\d*"}
         value={formData[name]}
         onChange={handleChange}
         className={`${styles.input} `}
@@ -20,7 +25,6 @@ const InputGroup = ({ name, formData, handleChange, errors, label }) => {
 
       {errors[name] && <span className={styles.error}>{errors[name]}</span>}
     </div>
-   
   );
 };
 export default InputGroup;
